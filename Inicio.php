089bed65b613e9a898f519c0274817a9f4d87574
PEDRO D'ANGELIS <pedrodangelis1@gmail.com>
2023-07-04 16:25:42 -0300

    inicio

--- Inicio.php
+++ Inicio.php
@@ -1,3 +1,8 @@
+<?php
+session_start();
+require 'db.php';
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -5,8 +10,18 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Início - VMH</title>
+    <link rel="stylesheet" href="stylesInicio.css">
 </head>
 <body>
+
+    <div id="AreaBranca">
+        <button id="botao"> Iniciar Questionário</button>   
+        
+    </div>
     
+    <a href="feedback.php" id="feedback">Feedback</a>
+
+    <div id="background"></div>
+
 </body>
 </html>
