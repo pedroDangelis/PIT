65eaef40faf562f2465606f27d170162e41c829d
PEDRO D'ANGELIS <pedrodangelis1@gmail.com>
2023-10-18 16:52:45 -0300

    as

--- apoio.php
+++ apoio.php
@@ -9,29 +9,24 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
   <title>Início - VMH</title>
-  <link rel="stylesheet" href="apoio.css">
+  <link rel="stylesheet" href="styleApoio.css">
   <link rel="preconnect" href="https://fonts.googleapis.com">
   <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
   <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
 </head>
 <body>
-
-  <header>
-    <div class="logo"><img src="./logo.png" alt=""></div>
-    <div class="nav-bar">
+<header>
+  <div class="logo"><img src="./logo.png" alt=""></div>
+  <div class="nav-bar">
       <ul class="nav-list">
-        <li class="list"><a class="nav" href="inicio.php">Home</a></li>
-        <li class="list"><a class="nav" href="index.php">Log IN</a></li>
-        <li class="list"><a class="nav" href="Cadastro.php">Sign UP</a></li>
+          <li class="list"><a class="nav" href="inicio.php">Home</a></li>
+          <li class="list"><a class="nav" href="index.php">Log IN</a></li>
+          <li class="list"><a class="nav" href="Cadastro.php">Sign UP</a></li>
       </ul>
-    </div>
-  </header>
+  </div>
+</header>
 
-  <aside>
-    <a href="Questionario.php"><button id="btn">Refazer Questionário!</button></a>
-    <p id="mensagens">Suas respostas foram registradas com sucesso! Obrigado pela confiança! <br><br> Você não está sozinho. Buscar ajuda é um sinal de coragem e cuidado. Estamos aqui para apoiar você no caminho da superação. <br> Respire fundo, confie e dê um passo de cada vez. Você vai vencer isso.</p>
   
-  </aside>
     
 </body>
 </html>
