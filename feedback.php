0c6543a083a173471d8078e3d777b14380b4b86e
PEDRO D'ANGELIS <pedrodangelis1@gmail.com>
2023-11-02 11:22:06 -0300

    asas

--- feedback.php
+++ feedback.php
@@ -38,7 +38,7 @@ if (isset($_POST["enviar"])) {
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>FeedBack</title>
-  <link rel="stylesheet" href="stylefeed.css">
+  <link rel="stylesheet" href="stylefeedback.css">
   <link rel="preconnect" href="https://fonts.googleapis.com">
   <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
   <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
@@ -77,6 +77,7 @@ if (isset($_POST["enviar"])) {
   </div>
 
   <div id="background"></div>
+  <div class="cont_dir"> <img src="" alt=""></div>
 
 </body>
 
